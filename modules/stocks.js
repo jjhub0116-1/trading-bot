5fbbe1575aa16d94ecd34acfacc37b632d8d11a2
Smith Samyuel <smithsamyuel004@gmail.com>
2026-04-15 20:17:07 +0530

    fix: expose all required stock fields from getAllStocks including stock_id, asset_type and lot_size

--- modules/stocks.js
+++ modules/stocks.js
@@ -20,10 +20,18 @@ async function getAllStocks() {
   try {
     const stocks = await Stock.find({});
     return stocks.map(s => ({
-      id: s.stock_id,
+      stock_id: s.stock_id,
       symbol: s.symbol,
-      name: s.stock_name,
-      price: s.current_price
+      stock_name: s.stock_name,
+      current_price: s.current_price,
+      asset_type: s.asset_type || 'STOCK',
+      lot_size: s.lot_size || 1,
+      fiftyTwoWeekHigh: s.fiftyTwoWeekHigh,
+      fiftyTwoWeekLow: s.fiftyTwoWeekLow,
+      dayHigh: s.dayHigh,
+      dayLow: s.dayLow,
+      previousClose: s.previousClose,
+      open: s.open
     }));
   } catch (err) {
     return [];
